9cbbe66bfdd36a70027a6c83f81b1d5181825683
Lenferd <DeysonSH@gmail.com>
2018-12-02 19:41:21 +0300

    Add second implementation

--- perf_test/perf_main.cpp
+++ perf_test/perf_main.cpp
@@ -1,5 +1,6 @@
 #include <benchmark/benchmark.h>
 #include "impl1.hpp"
+#include "impl2.hpp"
 #include "generator.hpp"
 
 static void IMPL1_Calculation(benchmark::State& state) {
@@ -16,5 +17,17 @@ static void IMPL1_Calculation(benchmark::State& state) {
 
 BENCHMARK(IMPL1_Calculation);
 
+static void IMPL2_Calculation(benchmark::State& state) {
+    using namespace impl_2;
+    double a[LENGTH];
+    my_vector v = {LENGTH, a};
+    double res;
+    simple_generator(a, LENGTH);
+
+    for (auto _ : state) {
+        foo_bar(&v, &res);
+    }
+}
+BENCHMARK(IMPL2_Calculation);
 
 BENCHMARK_MAIN();
